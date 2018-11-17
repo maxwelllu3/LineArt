a3078c3aebac62fef76f538d076bfc4a3826cf49
maxwelllu3 <blu@lcs.on.ca>
2018-11-17 11:44:48 -0500

    First Commit

---
+++ Playground/Playground.playground/Pages/SpiralStar.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: # With a tortoise 🐢
+//: [👉 With 2 tortoises 🐢🐢](@next)
+import PlaygroundSupport
+import TortoiseGraphics
+import CoreGraphics
+
+let myFrame = CGRect(x: 0, y: 0, width: 300, height: 300)
+let canvas = PlaygroundCanvas(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+canvas.frameRate = 60
+canvas.color = .white
+PlaygroundPage.current.liveView = canvas
+    
+
+
+canvas.drawing { turtle in
+    turtle.penColor(.black)
+
+    //Set Starting Position & Heading
+    turtle.penUp()
+    turtle.setPosition(-250, 65)
+    turtle.setHeading(90)
+    turtle.penDown()
+ 
+    //Start Drawing SpiralStar
+    turtle.spiralstar()
+    
+
+    
+
+}
--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -21,98 +21,64 @@ public extension Tortoise {
 
     }
 
-    func start() {
-
-        self.penUp()
-        self.goto(-350, -100)
-        self.setHeading(90)
+    func star(withSize size: Double) {
+        for _ in 1...5 {
+        self.forward(size)
+        self.right(145)
+        }
     }
 
-    func capitalM() {
-
-        self.penColor(.white)
-        self.penDown()
-        self.right(-100)
-        self.curve(withSides: 40, withSize: 15, drawSides: 5)
-        self.right(90)
-        self.curve(withSides: 30, withSize: 15, drawSides: 5)
-        self.right(160)
-        self.curve(withSides: 40, withSize: 15, drawSides: 5)
-        self.right(90)
-        self.curve(withSides: 30, withSize: 15, drawSides: 7)
-        print(self.heading)
-        self.setHeading(90)
-    }
-
-    func a() {
-
-        self.curve(withSides: -20, withSize: 10, drawSides: 5)
-        self.right(200)
-        self.curve(withSides: -20, withSize: 5, drawSides: 32)
-        self.right(200)
-        self.curve(withSides: -10, withSize: 10, drawSides: 5)
-    }
-
-    func x() {
-
-        self.curve(withSides: 30, withSize: 10, drawSides: 5)
-        self.right(120)
-        self.curve(withSides: -20, withSize: 15, drawSides: 5)
-        self.penUp()
-        self.setHeading(0)
-        self.forward(45)
-        self.right(-135)
-        self.penDown()
-        self.forward(60)
-        self.penUp()
-        self.setHeading(90)
-        self.penUp()
-        self.forward(42)
-        self.right(90)
-        self.forward(3)
-    }
-
-    func w() {
-
-        self.setHeading(96)
-        self.penDown()
-        self.curve(withSides: -20, withSize: 10, drawSides: 7)
-        self.right(180)
-        self.penDown()
-        self.forward(50)
-        self.right(-140)
-        self.forward(50)
-        self.right(140)
-        self.forward(50)
-        self.right(-140)
-        self.forward(50)
-    }
-
-    func e() {
-
-        self.right(150)
-        self.curve(withSides: -30, withSize: 12, drawSides: 5)
-        self.right(-40)
-        self.penDown()
-        self.forward(50)
-        self.right(-85)
-        self.curve(withSides: -10, withSize: 13, drawSides: 5)
-        self.curve(withSides: -10, withSize: 16, drawSides: 5)
-    }
-
-    func doublel() {
-
-        self.penDown()
-        self.right(10)
-        self.forward(30)
-        self.right(180)
-        self.forward(60)
-        self.right(-160)
-        self.forward(60)
-        self.right(150)
-        self.forward(70)
-        self.right(-140)
-        self.forward(20)
+    func spiralstar() {
+        self.star(withSize: 500)
+        self.star(withSize: 490)
+        self.star(withSize: 480)
+        self.star(withSize: 470)
+        self.star(withSize: 460)
+        self.star(withSize: 450)
+        self.star(withSize: 440)
+        self.star(withSize: 430)
+        self.star(withSize: 420)
+        self.star(withSize: 410)
+        self.star(withSize: 400)
+        self.star(withSize: 390)
+        self.star(withSize: 380)
+        self.star(withSize: 370)
+        self.star(withSize: 360)
+        self.star(withSize: 350)
+        self.star(withSize: 340)
+        self.star(withSize: 330)
+        self.star(withSize: 320)
+        self.star(withSize: 310)
+        self.star(withSize: 300)
+        self.star(withSize: 290)
+        self.star(withSize: 280)
+        self.star(withSize: 270)
+        self.star(withSize: 260)
+        self.star(withSize: 250)
+        self.star(withSize: 240)
+        self.star(withSize: 230)
+        self.star(withSize: 220)
+        self.star(withSize: 210)
+        self.star(withSize: 200)
+        self.star(withSize: 190)
+        self.star(withSize: 180)
+        self.star(withSize: 170)
+        self.star(withSize: 160)
+        self.star(withSize: 150)
+        self.star(withSize: 140)
+        self.star(withSize: 130)
+        self.star(withSize: 120)
+        self.star(withSize: 110)
+        self.star(withSize: 100)
+        self.star(withSize: 90)
+        self.star(withSize: 80)
+        self.star(withSize: 70)
+        self.star(withSize: 60)
+        self.star(withSize: 50)
+        self.star(withSize: 40)
+        self.star(withSize: 30)
+        self.star(withSize: 20)
+        self.star(withSize: 10)
     }
 
 }
