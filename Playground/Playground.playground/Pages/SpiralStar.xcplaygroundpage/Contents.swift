00c121a5294a3051f69a5ad015554728a5be9c6c
maxwelllu3 <blu@lcs.on.ca>
2018-11-20 15:17:28 -0500

    Second Commit

--- Playground/Playground.playground/Pages/SpiralStar.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/SpiralStar.xcplaygroundpage/Contents.swift
@@ -7,13 +7,13 @@ import CoreGraphics
 let myFrame = CGRect(x: 0, y: 0, width: 300, height: 300)
 let canvas = PlaygroundCanvas(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
 canvas.frameRate = 60
-canvas.color = .white
+canvas.color = .black
 PlaygroundPage.current.liveView = canvas
     
 
 
 canvas.drawing { turtle in
-    turtle.penColor(.black)
+    turtle.penColor(.white)
 
     //Set Starting Position & Heading
     turtle.penUp()
