30bf55fa1ed3532f4884bd1b5cab1449e83a7a84
maxwelllu3 <blu@lcs.on.ca>
2018-11-23 10:53:54 -0500

    Fourth Commit, fixed "Index Out of Range" error.

--- Playground/Playground.playground/Pages/SpiralStar.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/SpiralStar.xcplaygroundpage/Contents.swift
@@ -1,11 +1,9 @@
-//: # With a tortoise 🐢
-//: [👉 With 2 tortoises 🐢🐢](@next)
 import PlaygroundSupport
 import TortoiseGraphics
 import CoreGraphics
 
-let myFrame = CGRect(x: 0, y: 0, width: 300, height: 300)
-let canvas = PlaygroundCanvas(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+let myFrame = CGRect(x: 0, y: 0, width: 500, height: 500)
+let canvas = PlaygroundCanvas(frame: myFrame)
 canvas.frameRate = 60
 canvas.color = .white
 PlaygroundPage.current.liveView = canvas
